c7f251a1a0664c3f0450666d5d8beeae04d4f987
Shalabh Agarwal <agarwalshalabh98@gmail.com>
2024-03-10 20:57:47 +0530

    Added basic Homepage

---
+++ components/FeedCard/index.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+
+const FeedCard: React.FC = () => {
+  return (
+    <div className="border border-gray-600 p-4 border-r-0 border-l-0 border-b-0 hover:bg-slate-900 transition-all cursor-pointer">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-1">
+          <Image
+            src="https://avatars.githubusercontent.com/u/95584551?v=4"
+            alt="user-image"
+            height={50}
+            width={50}
+          />
+        </div>
+        <div className="col-span-11">
+          <h5>Shalabh Agarwal</h5>
+          <p>hello hey bro</p>
+          <div className="flex justify-between mt-5 text-xl items-center p-2 w-[90%]">
+            <div>
+              <BiMessageRounded />
+            </div>
+            <div>
+              <FaRetweet />
+            </div>
+            <div>
+              <AiOutlineHeart />
+            </div>
+            <div>
+              <BiUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedCard;
