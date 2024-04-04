e58dc4a0cff0c9f40ffd511e4602838a3e3fe93b
Shalabh Agarwal <agarwalshalabh98@gmail.com>
2024-04-05 02:24:10 +0530

    changes to api url

--- clients/api.ts
+++ clients/api.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 const isClient=typeof window!== 'undefined'
-export const graphqlClient = new GraphQLClient('https://d1d5j69xd736th.cloudfront.net/graphql', {
+export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL as string, {
   headers: () => ({
     Authorization: 
     isClient?`Bearer ${window.localStorage.getItem("__Twitter__token")}`:'undefined'
